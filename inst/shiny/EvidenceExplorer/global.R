b183d20551e2190c1077d12333fa7135602368b8
chandryou <applegna@gmail.com>
2019-02-20 19:04:36 +0900

    Revert "after running package maintenance"
    This reverts commit e74a0d27f4396f192d06be401ae1979286b13791.

--- inst/shiny/EvidenceExplorer/global.R
+++ inst/shiny/EvidenceExplorer/global.R
@@ -1,7 +1,7 @@
 source("DataPulls.R")
 source("PlotsAndTables.R")
 
-# shinySettings <- list(dataFolder = 'c:/temp/shinyDataNoPcs', blind = FALSE)
+# shinySettings <- list(dataFolder = "c:/temp/shinyDataNoPcs", blind = FALSE)
 dataFolder <- shinySettings$dataFolder
 blind <- shinySettings$blind
 connection <- NULL
@@ -12,7 +12,7 @@ splittableTables <- c("covariate_balance", "preference_score_dist", "kaplan_meie
 files <- list.files(dataFolder, pattern = ".rds")
 
 # Remove data already in global environment:
-tableNames <- gsub("(_t[0-9]+_c[0-9]+)|(_)[^_]*\\.rds", "", files)
+tableNames <- gsub("(_t[0-9]+_c[0-9]+)|(_)[^_]*\\.rds", "", files) 
 camelCaseNames <- SqlRender::snakeCaseToCamelCase(tableNames)
 camelCaseNames <- unique(camelCaseNames)
 camelCaseNames <- camelCaseNames[!(camelCaseNames %in% SqlRender::snakeCaseToCamelCase(splittableTables))]
@@ -21,7 +21,7 @@ rm(list = camelCaseNames)
 # Load data from data folder:
 loadFile <- function(file) {
   # file = files[13]
-  tableName <- gsub("(_t[0-9]+_c[0-9]+)|(_)[^_]*\\.rds", "", file)
+  tableName <- gsub("(_t[0-9]+_c[0-9]+)|(_)[^_]*\\.rds", "", file) 
   camelCaseName <- SqlRender::snakeCaseToCamelCase(tableName)
   if (!(tableName %in% splittableTables)) {
     newData <- readRDS(file.path(dataFolder, file))
@@ -38,3 +38,4 @@ lapply(files, loadFile)
 
 tcos <- unique(cohortMethodResult[, c("targetId", "comparatorId", "outcomeId")])
 tcos <- tcos[tcos$outcomeId %in% outcomeOfInterest$outcomeId, ]
+               
